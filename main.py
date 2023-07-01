4ba296ef95f0692519f942c63c0b83af10150b4a
Michael <drgnom95@gmail.com>
2023-07-01 10:53:03 +0300

    redevel bc videolern crash

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
-import pygame
+# import pygame
+from tkinter import *
 
-SIZE_BLOCK = 20
-FRAME_COLOR = (0, 255, 204)
-WHITE = (255, 255, 255)
-BLUE = (100, 255, 255)
-size = [400, 400]
-COUNT_BLOCKS = 20
-MARGIN = 1
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Змейка')
-while True:
+game_width = 500
+game_height = 500
+snake_item = 10
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-    screen.fill(FRAME_COLOR)
-    for row in range(COUNT_BLOCKS):
-        for column in range(COUNT_BLOCKS):
-            if (row + column) % 2 == 0:
-                color = BLUE
-            else:
-                color = WHITE
-            pygame.draw.rect(screen, color, [10 + column*SIZE_BLOCK + MARGIN * (column + 1),
-                                             20 + row*SIZE_BLOCK + MARGIN * (row + 1),
-                                             SIZE_BLOCK,
-                                             SIZE_BLOCK])
+tk = Tk()
+tk.title('Игра Змейка на Python')
+tk.resizable(0, 0)
+tk.wm_attributes('-topmost', 1)
+canvas = Canvas(tk, width=game_width, height=game_height, bd=0, highlightthickness=0)
+canvas.pack()
+tk.update()
 
-    pygame.display.flip()
+# SIZE_BLOCK = 20
+# FRAME_COLOR = (0, 255, 204)
+# WHITE = (255, 255, 255)
+# BLUE = (100, 255, 255)
+# size = [500, 600]
+# COUNT_BLOCKS = 20
+# MARGIN = 1
+# screen = pygame.display.set_mode(size)
+# pygame.display.set_caption('Змейка')
+# while True:
+#
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#     screen.fill(FRAME_COLOR)
+#     for row in range(COUNT_BLOCKS):
+#         for column in range(COUNT_BLOCKS):
+#             if (row + column) % 2 == 0:
+#                 color = BLUE
+#             else:
+#                 color = WHITE
+#             pygame.draw.rect(screen, color, [10 + column*SIZE_BLOCK + MARGIN * (column + 1),
+#                                              20 + row*SIZE_BLOCK + MARGIN * (row + 1),
+#                                              SIZE_BLOCK,
+#                                              SIZE_BLOCK])
+#
+#     pygame.display.flip()
