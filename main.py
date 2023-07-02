f42519cc583c4e1be2700421fbb41fe35e191a53
Michael <drgnom95@gmail.com>
2023-07-02 13:43:14 +0300

    add snake

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ from tkinter import *
 game_width = 500
 game_height = 500
 snake_item = 10
+snake_color1='red'
+snake_color2='yellow'
 
 tk = Tk()
 tk.title('Игра Змейка на Python')
-tk.resizable(0, 0)
+tk.resizable (0, 0)
 tk.wm_attributes('-topmost', 1)
 canvas = Canvas(tk, width=game_width, height=game_height, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
+def snake_paint_item(canvas, x, y):
+    canvas.create_rectangle(x*snake_item, y*snake_item,
+                            x*snake_item+snake_item, y*snake_item+snake_item,fill=snake_color1)
+    canvas.create_rectangle(x*snake_item+2, y*snake_item+2,
+                            x*snake_item+snake_item-2, y*snake_item+snake_item-2,fill=snake_color2)
+snake_paint_item(canvas, 1, 1)
 # SIZE_BLOCK = 20
 # FRAME_COLOR = (0, 255, 204)
 # WHITE = (255, 255, 255)
