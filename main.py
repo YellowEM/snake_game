35c56ba9a662e1b5a8ed0072949650e630af0706
Michael <drgnom95@gmail.com>
2023-07-05 19:17:31 +0300

    add presents for snake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # import pygame
 from tkinter import *
 import time
+import random
 
 game_width = 500
 game_height = 500
@@ -13,7 +14,11 @@ snake_x_nav = 0
 snake_y_nav = 0
 
 snake_list = []
-snake_size = 3
+snake_size = 5
+
+virtual_game_x = game_width / snake_item
+virtual_game_y = game_height / snake_item
+
 
 tk = Tk()
 tk.title("Игра Змейка на Python")
@@ -23,17 +28,41 @@ canvas = Canvas(tk, width=game_width, height=game_height, bd=0, highlightthickne
 canvas.pack()
 tk.update()
 
+present_color1 = 'blue'
+present_color2 = 'black'
+
+present_color1 = "blue"
+present_color2 = "black"
+presents_list = []
+presents_size = 5
+for i in range(presents_size):
+    x = random.randrange(virtual_game_x)
+    y = random.randrange(virtual_game_y)
+    id1 = canvas.create_oval(x*snake_item,y*snake_item, x*snake_item+snake_item,y*snake_item+snake_item,fill=present_color2)
+    id2 = canvas.create_oval(x*snake_item+2,y*snake_item+2,x*snake_item+snake_item-2,y*snake_item+snake_item-2,fill=present_color1)
+    presents_list.append([x, y, id1, id2])
+    presents_list.appen, ([x, y, id1, id2])
 
 def snake_paint_item(canvas, x, y):
     global snake_list
-    id1 = canvas.create_rectangle(x * snake_item, y * snake_item,
-                            x * snake_item + snake_item, y * snake_item + snake_item, fill=snake_color1)
-    id2 = canvas.create_rectangle(x * snake_item + 2, y * snake_item + 2,
-                            x * snake_item + snake_item - 2, y * snake_item + snake_item - 2, fill=snake_color2)
-    snake_list.append([x,y,id1,id2])
+    id1 = canvas.create_rectangle(x * snake_item, y * snake_item, x * snake_item + snake_item,
+                                  y * snake_item + snake_item, fill=snake_color2)
+    id2 = canvas.create_rectangle(x * snake_item + 2, y * snake_item + 2, x * snake_item + snake_item - 2,
+                                  y * snake_item + snake_item - 2, fill=snake_color1)
+    snake_list.append([x, y, id1, id2])
     print(snake_list)
 
+
 snake_paint_item(canvas, snake_x, snake_y)
+
+
+def check_can_we_delete_snake_item():
+    if len(snake_list) >= snake_size:
+        temp_item = snake_list.pop(0)
+        print(temp_item)
+        canvas.delete(temp_item[2])
+        canvas.delete(temp_item[3])
+
 
 def snake_move(event):
     global snake_x
@@ -41,23 +70,29 @@ def snake_move(event):
     if event.keysym == 'Up':
         snake_x_nav = 0
         snake_y_nav = -1
+        check_can_we_delete_snake_item()
     if event.keysym == 'Down':
         snake_x_nav = 0
         snake_y_nav = 1
+        check_can_we_delete_snake_item()
     if event.keysym == 'Left':
         snake_x_nav = -1
         snake_y_nav = 0
+        check_can_we_delete_snake_item()
     if event.keysym == 'Right':
         snake_x_nav = 1
         snake_y_nav = 0
+        check_can_we_delete_snake_item()
     snake_x = snake_x + snake_x_nav
     snake_y = snake_y + snake_y_nav
     snake_paint_item(canvas, snake_x, snake_y)
+
+
 canvas.bind_all("<KeyPress-Left>", snake_move)
 canvas.bind_all("<KeyPress-Right>", snake_move)
 canvas.bind_all("<KeyPress-Up>", snake_move)
 canvas.bind_all("<KeyPress-Down>", snake_move)
-
+tk.mainloop()
 
 # SIZE_BLOCK = 20
 # FRAME_COLOR = (0, 255, 204)
