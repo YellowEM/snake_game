a57b12c015bf7e39161a4840137f06116b65643f
Michael <drgnom95@gmail.com>
2023-07-06 21:55:54 +0300

    add center add level up snake add cycle for game(mechanics)

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ import random
 
 game_width = 500
 game_height = 500
-snake_item = 10
+snake_item = 20
 snake_color1 = 'red'
 snake_color2 = 'yellow'
-snake_x = 24
-snake_y = 24
+
+virtual_game_x = game_width // snake_item
+virtual_game_y = game_height // snake_item
+
+snake_x = virtual_game_x // 2
+snake_y = virtual_game_y // 2
 snake_x_nav = 0
 snake_y_nav = 0
 
 snake_list = []
 snake_size = 5
-
-virtual_game_x = game_width / snake_item
-virtual_game_y = game_height / snake_item
-
 
 tk = Tk()
 tk.title("Игра Змейка на Python")
@@ -34,12 +34,14 @@ present_color2 = 'black'
 present_color1 = "blue"
 present_color2 = "black"
 presents_list = []
-presents_size = 5
+presents_size = 25
 for i in range(presents_size):
     x = random.randrange(virtual_game_x)
     y = random.randrange(virtual_game_y)
-    id1 = canvas.create_oval(x*snake_item,y*snake_item, x*snake_item+snake_item,y*snake_item+snake_item,fill=present_color2)
-    id2 = canvas.create_oval(x*snake_item+2,y*snake_item+2,x*snake_item+snake_item-2,y*snake_item+snake_item-2,fill=present_color1)
+    id1 = canvas.create_oval(x * snake_item, y * snake_item, x * snake_item + snake_item, y * snake_item + snake_item,
+                             fill=present_color2)
+    id2 = canvas.create_oval(x * snake_item + 2, y * snake_item + 2, x * snake_item + snake_item - 2,
+                             y * snake_item + snake_item - 2, fill=present_color1)
     presents_list.append([x, y, id1, id2])
 
 
@@ -64,9 +66,20 @@ def check_can_we_delete_snake_item():
         canvas.delete(temp_item[3])
 
 
+def check_if_we_found_present():
+    global snake_size
+    for i in range(len(presents_list)):
+        if presents_list[i][0] == snake_x and presents_list[i][1] == snake_y:
+            snake_size += 1
+            canvas.delete(presents_list[i][2])
+            canvas.delete(presents_list[i][3])
+
+
 def snake_move(event):
     global snake_x
     global snake_y
+    global snake_x_nav
+    global snake_y_nav
     if event.keysym == 'Up':
         snake_x_nav = 0
         snake_y_nav = -1
@@ -86,12 +99,31 @@ def snake_move(event):
     snake_x = snake_x + snake_x_nav
     snake_y = snake_y + snake_y_nav
     snake_paint_item(canvas, snake_x, snake_y)
+    check_if_we_found_present()
 
 
 canvas.bind_all("<KeyPress-Left>", snake_move)
 canvas.bind_all("<KeyPress-Right>", snake_move)
 canvas.bind_all("<KeyPress-Up>", snake_move)
 canvas.bind_all("<KeyPress-Down>", snake_move)
+
+
+def check_if_borders():
+    if snake_x > virtual_game_x or snake_x < 0 or snake_y > virtual_game_y or snake_y < 0:
+        game_over()
+
+
+while 1:
+    check_can_we_delete_snake_item()
+    check_if_we_found_present()
+    check_if_borders()
+    snake_x = snake_x + snake_x_nav
+    snake_y = snake_y + snake_y_nav
+    snake_paint_item(canvas, snake_x, snake_y)
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.15)
+
 tk.mainloop()
 
 # SIZE_BLOCK = 20
